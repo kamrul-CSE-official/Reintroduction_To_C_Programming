c55a366962214678329b15982e6fbb62bac2c60c
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2026-04-12 01:07:59 +0600

    update module-5

---
+++ Module-5/char.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char a;
+    scanf("%c", &a);
+
+    if (a >= 'a' && a <= 'z')
+    {
+        a = a - 32;
+        printf("%c",a);
+    }
+    else
+    {
+        a = a + 32;
+        printf("%c",a);
+    }
+    return 0;
+}
