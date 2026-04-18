6cd97e9fe3fcdf2de51922e726f949f739e231bc
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2026-04-19 01:59:39 +0600

    Assignment-2 added

---
+++ Assignment-2/sum_sum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int ar[n];
+    int pos = 0;
+    int neg = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+
+        if (ar[i] >= 0)
+        {
+            pos += ar[i];
+        }
+        else
+        {
+            neg += ar[i];
+        }
+    }
+
+    printf("%d %d", pos, neg);
+
+    return 0;
+}
